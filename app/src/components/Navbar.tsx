2856865f14107be33f6b393017e3eb201cc1f904
mvykool <maykolandres4@gmail.com>
2024-05-31 12:52:59 -0500

    added base logic to navbar

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,31 +1,52 @@
+import { useState } from "react";
 
 
 export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+
   return (
-    <nav className="relative my-2 md:my-4 flex w-full justify-between items-center">
-      <span>Ambiet</span>
+    <header>
+      <nav className="relative my-2 md:my-4 flex w-full justify-between items-center">
+        <span>Ambiet</span>
 
-      {/*Mobile*/}
-      <button>
-        <i className='bx bx-menu md:hidden text-2xl'></i>
-      </button>
+        <div className={`fixed top-0 right-0 ${showMenu ? 'right-0' : '-right-28'}`}>
+          {/* Desktop */}
+          <ul className=" md:flex gap-14">
+            <li>
+              <a onClick={closeMenuOnMobile} href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Technologies</a>
+            </li>
+            <li>
+              <a href="#">Clients</a>
+            </li>
+          </ul>
+          <div className="">
+            <button onClick={toggleMenu}>close</button>
+          </div>
+        </div>
 
-      {/* Desktop */}
-      <ul className="hidden md:flex gap-14">
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Technologies</a>
-        </li>
-        <li>
-          <a href="#">Clients</a>
-        </li>
-      </ul>
-    </nav>
+        {/*Mobile*/}
+        <button onClick={toggleMenu}>
+          <i className='bx bx-menu md:hidden text-2xl'></i>
+        </button>
+      </nav>
+
+    </header>
   )
-}
+};
 
