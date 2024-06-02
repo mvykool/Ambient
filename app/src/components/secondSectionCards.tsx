a28365ba58c6b5518df18030d843d2db3bf4f92d
mvykool <maykolandres4@gmail.com>
2024-06-02 18:24:48 -0500

    fixed proportions

--- app/src/components/secondSectionCards.tsx
+++ app/src/components/secondSectionCards.tsx
@@ -3,9 +3,9 @@ import cardTwo from "../assets/secondsectiontwo.jpg"
 
 const SecondSectionCards = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center my-16 md:my-28">
+    <div className="flex flex-col md:flex-row gap-5 justify-center items-center w-full my-16 md:my-28">
       { /* Card 1 */}
-      <div className="flex h-40 w-full md:w-40 relative">
+      <div className="flex h-40 md:h-96 w-full md:w-2/6 relative">
         <img src={cardOne} className="absolute object-cover rounded-xl h-full w-full -z-10" alt="cardone" />
         <p>Follow us for more</p>
         <div>
@@ -15,12 +15,12 @@ const SecondSectionCards = () => {
         </div>
       </div>
       {/* Card 2 */}
-      <div className="bg-gray-200 w-full h-40 md:w-40">
+      <div className="bg-gray-200 rounded-xl w-full h-40 md:h-96 md:w-4/6">
         <h3>Join us in sculpting a green and sustainable world</h3>
         <button>Get Started</button>
       </div>
       {/* Card 3 */}
-      <div className="flex h-40 w-full md:w-40 relative">
+      <div className="flex h-40 md:h-96 w-full md:w-2/6 relative">
         <img src={cardTwo} className="absolute object-cover rounded-xl h-full w-full -z-10" alt="cartwo" />
         <i className="bx bxs-leaf"></i>
       </div>
