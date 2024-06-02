487ca77aca4a5ef6f1ee55cc06ce3fd240d676f2
mvykool <maykolandres4@gmail.com>
2024-06-02 15:29:13 -0500

    finished card with avatars

---
+++ app/src/components/avatars.tsx
@@ -0,0 +1,15 @@
+import personOne from '../assets/peopl1.jpg'
+import personTwo from '../assets/peopl2.jpg'
+import personThree from '../assets/peopl3.jpg'
+
+const Avatars = () => {
+  return (
+    <div className='flex -space-x-2 '>
+      <img className="inline-block h-14 w-14 ring-2 ring-white object-cover rounded-full" src={personOne} alt="person 1" />
+      <img className="inline-block h-14 w-14 ring-2 ring-white object-cover rounded-full" src={personTwo} alt="person 2" />
+      <img className="inline-block h-14 w-14 ring-2 ring-white object-cover rounded-full" src={personThree} alt="person 3 " />
+    </div>
+  )
+}
+
+export default Avatars
--- app/src/components/mediumCard.tsx
+++ app/src/components/mediumCard.tsx
@@ -1,8 +1,14 @@
+import Avatars from "./avatars"
 
 const MediumCard = () => {
   return (
-    <div className="bg-white h-48 w-48 rounded-xl relative">
+    <div className="bg-white h-48 w-48 rounded-xl relative p-4">
+      <Avatars />
 
+      <div className="mt-5 flex flex-col gap-2">
+        <h2 className="text-2xl">Pro Team</h2>
+        <p className="text-sm">We are here to help your project become green</p>
+      </div>
     </div >
   )
 }
