ef11e15caf9bf933ddc51024e1c01a32ae644697
mvykool <maykolandres4@gmail.com>
2024-06-02 20:51:12 -0500

    set up side cards

--- app/src/components/secondSectionCards.tsx
+++ app/src/components/secondSectionCards.tsx
@@ -5,13 +5,13 @@ const SecondSectionCards = () => {
   return (
     <div className="flex flex-col md:flex-row gap-5 justify-center items-center w-full my-16 md:my-28">
       { /* Card 1 */}
-      <div className="flex h-40 md:h-96 w-full md:w-2/6 relative">
+      <div className="flex flex-col justify-center items-center text-white h-40 md:h-96 w-full md:w-2/6 relative">
         <img src={cardOne} className="absolute object-cover rounded-xl h-full w-full -z-10" alt="cardone" />
-        <p>Follow us for more</p>
-        <div>
-          <i className="bx bxl-instagram-alt"></i>
-          <i className="bx bxl-facebook-square"></i>
-          <i className="bx bxl-twitter"></i>
+        <p className="text-lg text-center font-semibold">Follow us for more</p>
+        <div className="flex gap-2 my-4">
+          <i className="bx bxl-instagram-alt text-5xl"></i>
+          <i className="bx bxl-facebook-square text-5xl"></i>
+          <i className="bx bxl-twitter text-5xl"></i>
         </div>
       </div>
       {/* Card 2 */}
@@ -20,9 +20,9 @@ const SecondSectionCards = () => {
         <button>Get Started</button>
       </div>
       {/* Card 3 */}
-      <div className="flex h-40 md:h-96 w-full md:w-2/6 relative">
+      <div className="flex justify-center items-center h-40 md:h-96 w-full md:w-2/6 relative">
         <img src={cardTwo} className="absolute object-cover rounded-xl h-full w-full -z-10" alt="cartwo" />
-        <i className="bx bxs-leaf"></i>
+        <i className="bx bxs-leaf text-7xl text-white"></i>
       </div>
     </div>
   )
--- app/src/sections/Second.tsx
+++ app/src/sections/Second.tsx
@@ -4,7 +4,7 @@ const Second = () => {
   return (
     <>
       <main className="flex flex-col items-center mx-auto my-8 px-8 md:px-0">
-        <h2 className=" text-4xl text-center md:text-3xl lg:text-7xl w-4/6 mx-auto">
+        <h2 className=" text-4xl text-center md:text-3xl lg:text-7xl w-5/6 md:w-4/6 mx-auto">
           We're not just <span className="text-accent">building materials </span>- we're building a <span className="text-primary">legacy of change</span>
         </h2>
 
