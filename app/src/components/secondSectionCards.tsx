746c96f095646c7f592a11a749f0a7c2d22b48b8
mvykool <maykolandres4@gmail.com>
2024-06-02 21:56:39 -0500

    done with all cards

--- app/src/components/secondSectionCards.tsx
+++ app/src/components/secondSectionCards.tsx
@@ -15,9 +15,9 @@ const SecondSectionCards = () => {
         </div>
       </div>
       {/* Card 2 */}
-      <div className="bg-gray-200 rounded-xl w-full h-40 md:h-96 md:w-4/6">
-        <h3>Join us in sculpting a green and sustainable world</h3>
-        <button>Get Started</button>
+      <div className="bg-gray-100 flex flex-col p-5 md:p-10 items-center rounded-xl w-full h-40 md:h-96 md:w-4/6">
+        <h3 className="text-normal mt-0 md:mt-5 md:text-2xl text-center">Join us in sculpting <br /> a green and sustainable world</h3>
+        <button className="mt-6 md:mt-10 rounded-2xl bg-white px-3 md:px-5 py-2 md:py-3 text-sm md:text-normal">Get Started</button>
       </div>
       {/* Card 3 */}
       <div className="flex justify-center items-center h-40 md:h-96 w-full md:w-2/6 relative">
