0031fd594729a7b9bd6b7bdc2382069833cc556e
mvykool <maykolandres4@gmail.com>
2024-06-03 13:58:33 -0500

    changed bg of cards

--- app/src/components/cardParallax.tsx
+++ app/src/components/cardParallax.tsx
@@ -7,7 +7,7 @@ interface Props {
 const CardParallax = ({ title, content, image }: Props) => {
   return (
     <div className="h-screen flex justify-center items-center sticky top-0">
-      <div className="w-[700px] h-[300px] rounded-xl bg-red-300">
+      <div className="w-[700px] h-[300px] rounded-xl bg-primary">
         <p>{title}</p>
         <p>{content}</p>
         <img src={image} alt="" />
