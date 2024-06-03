0c0f56d99a91a942408d5afd4265f8a3667724e6
mvykool <maykolandres4@gmail.com>
2024-06-02 23:42:52 -0500

    added rest of pills

--- app/src/components/secondSectionCards.tsx
+++ app/src/components/secondSectionCards.tsx
@@ -19,7 +19,12 @@ const SecondSectionCards = () => {
         <h3 className="text-normal mt-0 md:mt-5 md:text-2xl text-center z-10">Join us in sculpting <br /> a green and sustainable world</h3>
         <button className="mt-6 md:mt-10 rounded-2xl bg-white z-10 px-3 md:px-5 py-2 md:py-3 text-sm md:text-normal">Get Started</button>
         {/* falling pills*/}
-        <div className="px-3 py-1 text-sm bg-green-200 absolute rounded-2xl top-20">Ethics</div>
+        <div className="px-3 py-1 text-sm bg-green-200 absolute rounded-2xl top-32">Ethics</div>
+        <div className="px-3 py-1 text-sm bg-green-200 absolute rounded-2xl top-40 left-28">Eco-friendly</div>
+        <div className="px-3 py-1 text-sm bg-green-200 absolute rounded-2xl top-5 right-52">Responsible</div>
+        <div className="px-3 py-1 text-sm bg-accent absolute rounded-2xl top-10">Progressive</div>
+        <div className="px-3 py-1 text-sm bg-accent absolute rounded-2xl top-28 right-4">Thoughtful</div>
+        <div className="px-3 py-1 text-sm bg-accent absolute rounded-2xl left-20 top-32">Sustainable</div>
       </div>
       {/* Card 3 */}
       <div className="flex justify-center items-center h-40 md:h-96 w-full md:w-2/6 relative">
