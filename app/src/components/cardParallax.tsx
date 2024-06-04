f43659df61588d80b0c43cc6f386dde57a2061e5
mvykool <maykolandres4@gmail.com>
2024-06-03 22:00:47 -0500

    framer motion image twiks

--- app/src/components/cardParallax.tsx
+++ app/src/components/cardParallax.tsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react'
+import { useScroll, motion, useTransform } from 'framer-motion'
+
 interface Props {
   title: string
   content: string
@@ -5,18 +8,27 @@ interface Props {
 }
 
 const CardParallax = ({ title, content, image }: Props) => {
+
+  const container = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'start start']
+  })
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [2, 1])
+
   return (
-    <div className="h-screen flex justify-center items-center sticky top-0">
+    <div ref={container} className="h-screen flex justify-center items-center sticky top-0">
       <div className="w-[300px] md:w-[700px] h-[170px] md:h-[300px] relative -top-[10%] rounded-xl bg-primary">
         <p>{title}</p>
         <p>{content}</p>
-        <div>
-          <div>
-            <img className="w-1/2" src={image} alt="" />
-          </div>
+        <div className="relative w-[60%] h-[100%] rounded-xl overflow-hidden">
+          <motion.div style={{ scale: scaleProgress }} className="w-[100%] h-[100%]">
+            <img className="w-1/2 object-cover rounded-xl" src={image} alt="" />
+          </motion.div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
