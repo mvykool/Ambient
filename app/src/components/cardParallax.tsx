3d69c4e0e707f2cf0ddce61c1d382a77485f4ff9
mvykool <maykolandres4@gmail.com>
2024-06-03 21:41:54 -0500

    added images

--- app/src/components/cardParallax.tsx
+++ app/src/components/cardParallax.tsx
@@ -7,10 +7,14 @@ interface Props {
 const CardParallax = ({ title, content, image }: Props) => {
   return (
     <div className="h-screen flex justify-center items-center sticky top-0">
-      <div className="w-[300px] md:w-[700px] h-[170px] md:h-[300px] rounded-xl bg-primary">
+      <div className="w-[300px] md:w-[700px] h-[170px] md:h-[300px] relative -top-[10%] rounded-xl bg-primary">
         <p>{title}</p>
         <p>{content}</p>
-        <img src={image} alt="" />
+        <div>
+          <div>
+            <img className="w-1/2" src={image} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
--- app/src/data.ts
+++ app/src/data.ts
@@ -1,17 +1,21 @@
+import one from '../src/assets/parallaxone.jpg'
+import two from '../src/assets/parallaxtwo.jpg'
+import three from '../src/assets/parallaxthree.jpg'
+
 export const cards = [
   {
     title: 'Card 1',
     content: 'This is card 1',
-    image: ''
+    image: `${one}`
   },
   {
     title: 'Card 2',
     content: 'This is card 2',
-    image: ''
+    image: `${two}`
   },
   {
     title: 'Card 3',
     content: 'This is card 3',
-    image: ''
+    image: `${three}`
   }
 ]
