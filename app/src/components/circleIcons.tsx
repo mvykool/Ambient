3ff6d7fdf20a4a7dd4e98e0ee9e631bc97a46f10
mvykool <maykolandres4@gmail.com>
2024-06-04 23:32:42 -0500

    motioned icons

--- app/src/components/circleIcons.tsx
+++ app/src/components/circleIcons.tsx
@@ -1,12 +1,40 @@
+import { useRef } from 'react'
+import { useInView } from 'framer-motion'
 
-const circleIcons = () => {
+const CircleIcons = () => {
+  // Framer motion
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+
   return (
     <div className="flex gap-3">
-      <i className="bx bxs-tree text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
-      <i className="bx bxs-sun text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
-      <i className="bx bxs-droplet text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
+      <i
+        ref={ref}
+        style={{
+          transform: isInView ? "none" : "translateX(-100px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.4s"
+        }}
+        className="bx bxs-tree text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
+      <i
+        ref={ref}
+        style={{
+          transform: isInView ? "none" : "translateX(-100px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
+        }}
+        className="bx bxs-sun text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
+      <i
+        ref={ref}
+        style={{
+          transform: isInView ? "none" : "translateX(-100px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s"
+        }}
+        className="bx bxs-droplet text-xl bg-primary rounded-full w-8 h-8 flex items-center justify-center hover:scale-110 transition transform duration-100 hover:shadow-md"></i>
     </div>
   )
 }
 
-export default circleIcons
+export default CircleIcons
