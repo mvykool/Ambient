e4172adb826de5b09447569f8e1643626f993cf2
mvykool <maykolandres4@gmail.com>
2024-06-16 23:56:13 -0500

    optimized avatars

--- app/src/components/avatars.tsx
+++ app/src/components/avatars.tsx
@@ -5,9 +5,25 @@ import personThree from '../assets/peopl3.webp'
 const Avatars = () => {
   return (
     <div className='flex -space-x-2 '>
-      <img loading="lazy" className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md" src={personOne} alt="person 1" />
-      <img loading="lazy" className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md" src={personTwo} alt=" person 2" />
-      <img loading="lazy" className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md" src={personThree} alt="person 3 " />
+      <img
+        loading="lazy"
+        className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md"
+        srcSet={`${personOne}?w=100 100w, ${personOne}?w=200 200w, ${personOne}?w=400 400w, ${personOne}?w=800 800w`}
+        sizes="(max-width: 800px) 100vw, 50vw"
+        decoding="async"
+        src={personOne} alt="person 1" />
+      <img loading="lazy"
+        className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md"
+        srcSet={`${personTwo}?w=100 100w, ${personTwo}?w=200 200w, ${personTwo}?w=400 400w, ${personTwo}?w=800 800w`}
+        sizes="(max-width: 800px) 100vw, 50vw"
+        decoding="async"
+        src={personTwo} alt=" person 2" />
+      <img loading="lazy"
+        className="inline-block h-10 w-10 md:h-14 md:w-14 ring-2 ring-white object-cover rounded-full hover:scale-110 transition transform duration-100 hover:shadow-md"
+        srcSet={`${personThree}?w=100 100w, ${personThree}?w=200 200w, ${personThree}?w=400 400w, ${personThree}?w=800 800w`}
+        sizes="(max-width: 800px) 100vw, 50vw"
+        decoding="async"
+        src={personThree} alt="person 3 " />
     </div>
   )
 }
