db186f3490e3ec6f21e897cfe792f6417b5a5e73
mvykool <maykolandres4@gmail.com>
2024-06-16 23:47:43 -0500

    optimized imgs

--- app/src/components/smallCard.tsx
+++ app/src/components/smallCard.tsx
@@ -4,7 +4,7 @@ interface Props {
 const SmallCard = ({ imageUrl }: Props) => {
   return (
     <div >
-      <img src={imageUrl} alt="card" loading="lazy" className="rounded-xl h-[12rem] w-[9rem] object-cover hover:scale-110 transform transition hover:shadow-lg" />
+      <img srcSet={imageUrl} width={120} height={180} alt="card" loading="lazy" className="rounded-xl hover:scale-110 transform transition hover:shadow-lg" />
     </div>
   )
 }
