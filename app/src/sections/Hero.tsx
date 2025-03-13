c8e09162a6ab9a1e92584aafaab5655c688ee3e0
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-13 02:32:56 -0400

    Updated animation

--- app/src/sections/Hero.tsx
+++ app/src/sections/Hero.tsx
@@ -1,19 +1,18 @@
-import { useEffect, useState, useRef } from "react"
-import heroImg from "../assets/main.webp"
-import cardOne from "../assets/one.webp"
-import cardTwo from "../assets/main-hero.webp"
+import { useEffect, useState, useRef } from "react";
+import heroImg from "../assets/main.webp";
+import cardOne from "../assets/one.webp";
+import cardTwo from "../assets/main-hero.webp";
 import SmallCard from "../components/smallCard";
 import CircleIcons from "../components/circleIcons";
 import MediumCard from "../components/mediumCard";
-import { useInView } from 'framer-motion';
+import { useInView } from "framer-motion";
 
 const Hero = () => {
-
-  const [colorClass, setColorClass] = useState('text-black');
+  const [colorClass, setColorClass] = useState("text-black");
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setColorClass('text-primary');
+      setColorClass("text-primary");
     }, 1000);
 
     // Cleanup timer if component unmounts
@@ -22,12 +21,11 @@ const Hero = () => {
 
   // Framer motion
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true })
+  const isInView = useInView(ref, { once: true });
 
   return (
     <>
       <main className="flex flex-col md:flex-row items-center md:items-start pt-20 md:pt-36 mx-auto px-8 md:px-0 min-h-[100vh]">
-
         {/* Main section text */}
 
         <div
@@ -35,17 +33,29 @@ const Hero = () => {
           style={{
             transform: isInView ? "none" : "translateX(-100px)",
             opacity: isInView ? 1 : 0,
-            transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s"
+            transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
           }}
-          className="flex flex-col justify-center relative w-full md:w-3/6">
+          className="flex flex-col justify-center relative w-full md:w-3/6"
+        >
+          <span className=" font-light text-gray-700  md:font-semibold md:text-gray-900">
+            Sculpting the Future with Green Materials
+          </span>
 
-          <span className=" font-light text-gray-700  md:font-semibold md:text-gray-900">Sculpting the Future with Green Materials</span>
-
-          <h1 className=" mt-5 font-bold text-4xl text-left md:text-3xl lg:text-7xl md:font-normal md:flex-wrap ">Leading the Way in <span className={`${colorClass} transition-colors duration-500`}>Sustainable</span>  Materials</h1>
+          <h1 className=" mt-5 font-bold text-4xl text-left md:text-3xl lg:text-7xl md:font-normal md:flex-wrap ">
+            Leading the Way in{" "}
+            <span className={`${colorClass} transition-colors duration-500`}>
+              Sustainable
+            </span>{" "}
+            Materials
+          </h1>
 
           <div className="flex justify-between md:justify-start gap-0 md:gap-5 mt-10">
-            <button className="px-3 md:px-4 py-2 rounded-lg border border-gray-200 hover:bg-gray-200 hover:scale-105 transition transform duration-100">Selected Projects</button>
-            <button className="px-3 md:px-4 py-2 rounded-lg border border-gray-200 hover:bg-gray-200 hover:scale-105 transition transform duration-100">Work with Us</button>
+            <button className="px-3 md:px-4 py-2 rounded-lg border border-gray-200 hover:bg-gray-200 hover:scale-105 transition transform duration-100">
+              Selected Projects
+            </button>
+            <button className="px-3 md:px-4 py-2 rounded-lg border border-gray-200 hover:bg-gray-200 hover:scale-105 transition transform duration-100">
+              Work with Us
+            </button>
           </div>
         </div>
 
@@ -55,27 +65,29 @@ const Hero = () => {
           <img
             srcSet={`${heroImg}?w=100 100w, ${heroImg}?w=200 200w, ${heroImg}?w=400 400w, ${heroImg}?w=800 800w`}
             sizes="(max-width: 800px) 100vw, 50vw"
-            decoding="async"
             fetchPriority="high"
             role="presentation"
             src={heroImg}
             alt="hero-img"
-            ref={ref} style={{
-              transform: isInView ? "none" : "translateY(-100px)",
+            ref={ref}
+            style={{
+              transform: isInView ? "none" : "translateX(100px)",
               opacity: isInView ? 1 : 0,
-              transition: "all 0.6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.4s"
+              transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
             }}
             className="rounded-xl h-[90vh] object-cover w-full"
           />
         </div>
 
-        <div ref={ref}
+        <div
+          ref={ref}
           style={{
             transform: isInView ? "none" : "translateX(100px)",
             opacity: isInView ? 1 : 0,
-            transition: "all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.4s"
+            transition: "all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.4s",
           }}
-          className="relative md:absolute right-0 md:right-10 bottom-0 mb-7 mt-16 md:my-[8%]">
+          className="relative md:absolute right-0 md:right-10 bottom-0 mb-7 mt-16 md:my-[8%]"
+        >
           <MediumCard />
         </div>
 
@@ -85,11 +97,12 @@ const Hero = () => {
           <CircleIcons />
 
           <div className="flex gap-8">
-            <div ref={ref}
+            <div
+              ref={ref}
               style={{
                 transform: isInView ? "none" : "translateX(-100px)",
                 opacity: isInView ? 1 : 0,
-                transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
               }}
             >
               <SmallCard imageUrl={cardOne} />
@@ -98,7 +111,7 @@ const Hero = () => {
               style={{
                 transform: isInView ? "none" : "translateX(-100px)",
                 opacity: isInView ? 1 : 0,
-                transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
+                transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
               }}
             >
               <SmallCard imageUrl={cardTwo} />
@@ -107,7 +120,7 @@ const Hero = () => {
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
